a5a54b90d54ddb073ef21bf324f635aed2764c7f
Yanuar Nurcahyo <yanuar.nurcahyo6661@gmail.com>
2015-10-14 20:51:51 +0700

    Ada yang baru di /doc/dbagendaris.sql

--- doc/dbagendaris.sql
+++ doc/dbagendaris.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 21, 2015 at 10:51 AM
+-- Generation Time: Oct 14, 2015 at 03:51 PM
 -- Server version: 5.6.14
 -- PHP Version: 5.5.6
 
@@ -65,6 +65,8 @@ CREATE TABLE IF NOT EXISTS `leader_mails` (
   `mail_inbox_id` int(11) NOT NULL,
   `leader_id` int(11) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0' COMMENT '0 = Not Approved; 1 = Approved',
+  `done` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`leader_id`,`mail_inbox_id`),
   KEY `fk_leader_unit_mails_leaders1_idx` (`leader_id`),
   KEY `mail_inbox_id` (`mail_inbox_id`,`leader_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -73,13 +75,12 @@ CREATE TABLE IF NOT EXISTS `leader_mails` (
 -- Dumping data for table `leader_mails`
 --
 
-INSERT INTO `leader_mails` (`mail_inbox_id`, `leader_id`, `status`) VALUES
-(3, 3, 1),
-(3, 7, 1),
-(3, 1, 0),
-(3, 7, 0),
-(5, 3, 0),
-(5, 1, 0);
+INSERT INTO `leader_mails` (`mail_inbox_id`, `leader_id`, `status`, `done`) VALUES
+(7, 1, 0, 0),
+(6, 3, 0, 0),
+(7, 3, 1, 0),
+(6, 7, 0, 0),
+(7, 7, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -107,16 +108,95 @@ CREATE TABLE IF NOT EXISTS `mail_inbox` (
   `leader_unit_status` int(11) NOT NULL DEFAULT '0' COMMENT '0 = Disabled; 1 = Enabled',
   PRIMARY KEY (`id`,`leader_id`,`mail_type`),
   KEY `fk_mail_inbox_leaders1_idx` (`leader_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Dumping data for table `mail_inbox`
 --
 
 INSERT INTO `mail_inbox` (`id`, `leader_id`, `mail_type`, `no_surat`, `no_arsip`, `lampiran`, `perihal`, `asal_surat`, `message_leader`, `message_leader_assistant`, `file`, `created`, `received_date`, `limit_date`, `leader_status`, `leader_assistant_status`, `leader_unit_status`) VALUES
-(2, 2, 1, '012348909', '09345908430958', 'Controller''s constructor now takes two parameters', 'A CakeRequest, and CakeResponse objects', 'PT. Telkomnesia', 'These objects are used to populate several deprecated properties and will be set to $request and $re- sponse inside the controller. ', 'c,kd', '', '2015-08-24 15:38:38', '2015-08-24', '2015-08-24', 0, 0, 0),
-(3, 2, 1, '001', '002', 'The second parameter indicates the length of the time-intervals between each execution.', 'The setInterval() Method', 'w3school', 'Jancok', 'Cuk gogo power ranger', '', '2015-09-09 15:18:28', '2015-09-09', '2015-09-09', 1, 0, 0),
-(5, 2, 3, '03293', '04589', 'Creates a select element populated with the years from $minYear to $maxYear.', 'Creating date and time inputs', 'CakePHP Developer', 'panjangkan otong mu', 'tancap gas', '', '2015-09-14 21:53:27', '2015-01-15', '2015-04-12', 0, 0, 0);
+(6, 2, 2, 'A.001/Pan-Pel/A', 'AS-0001', '-', 'Praise for Michael Hartlâ€™s', 'Ruby On Rail', 'Models are part of the MVC\r\n9\r\narchitecture. They are objects representing\r\nbusiness data, rules and logic.\r\nYou can create model classes by extending yii\\base\\Model or its child\r\nclasses. The base class yii\\base\\Model supports many useful features:', 'â€˜My former company (CD Baby) was one of the ï¬rst to loudly switch to Ruby on Rails, and then even more loudly switch back to PHP (Google me to read about the drama). This book by Michael Hartl came so highly recommended that I had to try it, and the Ruby on RailsTMTutorial is what I used to switch back to Rails again.', '187559_340.jpg', '2015-09-30 12:50:16', '2015-02-16', '2015-02-13', 1, 0, 0),
+(7, 2, 2, 'DK30/13', 'AS-0002', 'Lampiran', '-', 'SMK ABC', 'Pesan ku', 'Unittt', '1378695_578693538857791_1227148412_n.jpg', '2015-10-08 14:23:25', '2015-06-29', '2002-04-09', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE IF NOT EXISTS `notifications` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content_id` int(11) NOT NULL,
+  `leader_id` int(11) NOT NULL,
+  `content` varchar(45) NOT NULL,
+  `action` varchar(45) NOT NULL,
+  `redirect` varchar(45) NOT NULL,
+  `mail_status` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0 =Unread, 1 = Read',
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`,`content_id`,`leader_id`),
+  KEY `fk_notifications_outboxes1_idx` (`content_id`),
+  KEY `fk_notifications_leaders1_idx` (`leader_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `content_id`, `leader_id`, `content`, `action`, `redirect`, `mail_status`, `status`, `created`) VALUES
+(4, 6, 3, 'outboxes', 'approved', 'read', 1, 0, '2015-10-14 18:24:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outboxes`
+--
+
+CREATE TABLE IF NOT EXISTS `outboxes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `no_arsip` varchar(15) NOT NULL,
+  `lampiran` varchar(100) DEFAULT NULL,
+  `perihal` varchar(100) DEFAULT NULL,
+  `mail_type` int(11) NOT NULL COMMENT '1 = Segera, 2 = Penting, 3 = Biasa, 4 = Rahasia, 5 = Pribadi',
+  `pesan` text,
+  `file` varchar(145) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Dumping data for table `outboxes`
+--
+
+INSERT INTO `outboxes` (`id`, `no_arsip`, `lampiran`, `perihal`, `mail_type`, `pesan`, `file`, `created`) VALUES
+(5, 'OX-0001', 'ljkj', 'lkj', 2, NULL, NULL, '2015-10-03 15:48:50'),
+(6, 'OX-0001', 'Official CakePHP discussion group', 'CakePHP also has its official discusson group on Google Groups. There are thousands of people discus', 1, NULL, '6270027_20140609115024.jpg', '2015-10-03 15:49:09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outbox_leaders`
+--
+
+CREATE TABLE IF NOT EXISTS `outbox_leaders` (
+  `outbox_id` int(11) NOT NULL,
+  `leader_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '0 = Not Approve; 1 = Approved',
+  PRIMARY KEY (`outbox_id`,`leader_id`),
+  KEY `fk_outbox_leaders_leaders1_idx` (`leader_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `outbox_leaders`
+--
+
+INSERT INTO `outbox_leaders` (`outbox_id`, `leader_id`, `status`) VALUES
+(5, 1, 0),
+(5, 3, 1),
+(6, 1, 0),
+(6, 3, 1),
+(6, 13, 0),
+(6, 14, 0);
 
 -- --------------------------------------------------------
 
@@ -168,13 +248,28 @@ INSERT INTO `users` (`id`, `leader_id`, `username`, `password`, `display_name`, 
 --
 ALTER TABLE `leader_mails`
   ADD CONSTRAINT `fk_leader_unit_mails_leaders1` FOREIGN KEY (`leader_id`) REFERENCES `leaders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_unit_leader_mails_mail_inbox1` FOREIGN KEY (`mail_inbox_id`) REFERENCES `mail_inbox` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_unit_leader_mails_mail_inbox1` FOREIGN KEY (`mail_inbox_id`) REFERENCES `mail_inbox` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `mail_inbox`
 --
 ALTER TABLE `mail_inbox`
   ADD CONSTRAINT `fk_mail_inbox_leaders1` FOREIGN KEY (`leader_id`) REFERENCES `leaders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `fk_notifications_leaders1` FOREIGN KEY (`leader_id`) REFERENCES `leaders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_notifications_mail_inbox1` FOREIGN KEY (`content_id`) REFERENCES `mail_inbox` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_notifications_outboxes1` FOREIGN KEY (`content_id`) REFERENCES `outboxes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `outbox_leaders`
+--
+ALTER TABLE `outbox_leaders`
+  ADD CONSTRAINT `fk_outbox_leaders_leaders1` FOREIGN KEY (`leader_id`) REFERENCES `leaders` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_outbox_leaders_outboxes1` FOREIGN KEY (`outbox_id`) REFERENCES `outboxes` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `users`
